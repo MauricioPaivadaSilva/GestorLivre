be304a90d0c1cf8bd02e99ccbe5299fc5809b474
Maurício Paiva da Silva <mauriciopaiva.matematica@gmail.com>
2025-01-01 12:59:03 -0300

    Feat/4 - Criação da interface gráfica
    Não iniciado. Commit de início de repositório.

---
+++ main.py
@@ -0,0 +1,13 @@
+# A interface gráfica será dividida em duas partes, uma para a pesquisa e outra para o registro.
+
+# Na pesquisa haverá um campo para a inserção dos termos que condizirão a busca no banco de dados, e esse campo ficara sentralizado, com um botão de pesquisar ao lado.
+
+# No cadastro haverá campos para a inserção dos dados e no final da página haverá um botão de cadastrar. Após cadastrar, uma pop-up deve aparecer dizendo se o cadastro foi concluído e qual o id do documento.
+
+# Importante: Deve haver um seletor de tipo de documento (livro, artigo, revista,...)
+
+import tkinter as tk
+
+root = tk.Tk()
+
+root.mainloop()
