e96b375d0cf7ee00485dfe9ba536213922aebe92
Maurício Paiva da Silva <mauriciopaiva.matematica@gmail.com>
2025-01-03 15:02:56 -0300

    Feat/4 - Criação da interface gráfica
    Criada a tela de cadastro com validação dos dados inseridos. O funcionamento é simples e básico, mas é funcional. Ainda é necessário refinamento.
    
    Closes #4.

--- main.py
+++ main.py
@@ -7,6 +7,7 @@
 # Importante: Deve haver um seletor de tipo de documento (livro, artigo, revista,...)
 
 import tkinter as tk
+from tkinter import ttk
 from functools import partial
 
 from src.connect import connect
@@ -63,6 +64,92 @@ def window_find():
 def window_register():
     clear()
     menu()
+
+    fontB = "Times 12 bold"
+    font = "Times 12"
+
+    options = ["Livro", "Artigo", "Periódico"]
+
+    root.configure(bg=LIGHTPURPLE)
+
+    def vf(): 
+        ts = all([titleInfo.get(), authorInfo.get(), editionInfo.get(), editorInfo.get(), localInfo.get(), dateInfo.get(), tpInfo.get()])
+        return ts 
+
+    def send():
+
+        if vf() == True:
+            connect(authorInfo.get(), titleInfo.get(), editionInfo.get(), localInfo.get(), editorInfo.get(), dateInfo.get(), tpInfo.get())
+
+            verify = connect.find(term=titleInfo.get())
+            if (titleInfo.get() == verify[0][1]) and (tpInfo.get() == verify[0][8]):
+                popup = tk.Toplevel()
+                popup.title("Conluído")
+                popup.minsize(500, 200)
+                popup.maxsize(500, 200)
+                content = tk.Message(popup, text=f"{tpInfo.get()} {titleInfo.get()} cadastrado!\nChamada: {verify[0][0]}", width=450)
+                content.pack(pady=10)
+
+                closed = tk.Button(popup, text="Fechar", command=popup.destroy)
+                closed.pack(pady=10)
+            else:
+                popup = tk.Toplevel()
+                popup.title("Erro")
+                popup.minsize(500, 200)
+                popup.maxsize(500, 200)
+                content = tk.Message(popup, text=f"{tpInfo.get()} {titleInfo.get()} Não cadastrado!", width=450)
+                content.pack(pady=10)
+
+                closed = tk.Button(popup, text="Fechar", command=popup.destroy)
+                closed.pack(pady=10)
+
+        else:
+            popup = tk.Toplevel()
+            popup.title("Erro")
+            popup.minsize(300, 100)
+            popup.maxsize(300, 100)
+            content = tk.Message(popup, text="Por favor, preencha todos os campos.", width=250)
+            content.pack(pady=10)
+            closed = tk.Button(popup, text="Fechar", command=popup.destroy)
+            closed.pack(pady=10)
+
+    title = tk.Label(root, text="Título:", font=font, bg=LIGHTPURPLE)
+    title.grid(row=0, column=0, padx=10, pady=5)
+    titleInfo = tk.Entry(root, font=font, width=50)
+    titleInfo.grid(row=0, column=1, padx=10, pady=5)
+
+    author = tk.Label(root, text="Autor:", font=font, bg=LIGHTPURPLE)
+    author.grid(row=1, column=0, padx=10, pady=5)
+    authorInfo = tk.Entry(root, font=font, width=50)
+    authorInfo.grid(row=1, column=1, padx=10, pady=5)
+
+    edition = tk.Label(root, text="Edição:", font=font, bg=LIGHTPURPLE)
+    edition.grid(row=2, column=0, padx=10, pady=5)
+    editionInfo = tk.Entry(root, font=font, width=50)
+    editionInfo.grid(row=2, column=1, padx=10, pady=5)
+
+    editor = tk.Label(root, text="Editora:", font=font, bg=LIGHTPURPLE)
+    editor.grid(row=3, column=0, padx=10, pady=5)
+    editorInfo = tk.Entry(root, font=font, width=50)
+    editorInfo.grid(row=3, column=1, padx=10, pady=5)
+
+    local = tk.Label(root, text="Local:", font=font, bg=LIGHTPURPLE)
+    local.grid(row=4, column=0, padx=10, pady=5)
+    localInfo = tk.Entry(root, font=font, width=50)
+    localInfo.grid(row=4, column=1, padx=10, pady=5)
+
+    date = tk.Label(root, text="Data:", font=font, bg=LIGHTPURPLE)
+    date.grid(row=5, column=0, padx=10, pady=5)
+    dateInfo = tk.Entry(root, font=font, width=50)
+    dateInfo.grid(row=5, column=1, padx=10, pady=5)
+
+    tp = tk.Label(root, text="Tipo:", font=font, bg=LIGHTPURPLE)
+    tp.grid(row=6, column=0, padx=10, pady=5)
+    tpInfo = ttk.Combobox(root, values=options)
+    tpInfo.grid(row=6, column=1, padx=10, pady=5)
+
+    sendB = tk.Button(root, text="Cadastrar", font=font, command=send)
+    sendB.grid(row=7, column=1, padx=10, pady=5)
 
 window_find()
 
